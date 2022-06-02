00e7499a2805aca2b1ff5ac88088420ef9e5b204
primovere <p.8izo3@slmail.me>
2022-06-02 17:15:48 +0800

    Add the function playRound() and a comment
    The function plays a single round of Rock Paper Scissors. Currently it
    can only check if the user wins the game.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ let num;
 function computerPlay() {
   // ramdomly generate a number between 0 and 2 (both inclusive)
   num = Math.floor(Math.random() * 3);
+
+  // each number returns a corresponding string
   switch (num) {
     case 0:
       return "Rock";
@@ -13,4 +15,33 @@ function computerPlay() {
     case 2:
       return "Scissors";
     } 
+}
+
+function playRound(playerSelection=a, computerSelection=computerPlay()) {
+  if (playerSelection == "Rock") {
+    if (computerSelection == "Rock") {
+      return "Draw";
+    } else if (computerSelection == "Paper") {
+      return "You Lose! Paper beats Rock.";
+    } else if (computerSelection == "Scissors") {
+      return "You Win!";
+    }
+  } else if (playerSelection == "Paper") {
+    if (computerSelection == "Rock") {
+      return "You Win!";
+    } else if (computerSelection == "Paper") {
+      return "Draw";
+    } else if (computerSelection == "Scissors") {
+      return "You Lose! Scissors beats Paper";
+    }
+  } else if (playerSelection == "Scissors") {
+    if (computerSelection == "Rock") {
+      return "You Lose! Rock beats Scissors.";
+    } else if (computerSelection == "Paper") {
+      return "You Win!";
+    } else if (computerSelection == "Scissors") {
+      return "Draw";
+    }
+  }
+
 }
